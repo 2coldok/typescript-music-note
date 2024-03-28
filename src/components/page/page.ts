a593e28817ddc55d919244dd3d6d680a2e41db7f
coldok <2coldok@gmail.com>
2024-03-29 01:03:59 +0900

    feat : item 삭제기능 구현

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -8,7 +8,11 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
+type OnCloseListener = () => void;
+
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+  private closeListener?: OnCloseListener;
+
   constructor() {
     super(`<li class="page-item">
              <section class="page-item__body"></section>
@@ -16,12 +20,20 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
                <button class="close">&times;</button>
              </div>
           </li>`)
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    }      
   }
 
   // child : Component interface를 구현하는 어떤 class 던 가능 
   addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container); // li속 section자리에 child 삽입
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -33,6 +45,9 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
   addChild(section: Component) {
     const item = new PageItemComponent();
     item.addChild(section);
-    item.attachTo(this.element, 'beforeend');  
+    item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });  
   }
 }
